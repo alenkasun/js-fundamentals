88ceb77fd6a01e6011b6033780af1ec333014899
Alyona Shevchenko <fiery.dragon12@gmail.com>
2016-02-10 18:03:18 +0200

    dive into scope

--- lesson-5/script.js
+++ lesson-5/script.js
@@ -1,11 +1,16 @@
 var globalVar = 'This is global variable';
 
-var globalFunction = function(){
-	var localVar = 'This is local variable';
+var globalFunction = function(arg){
+	var localVar = 'This is local variable.';
+	var localFunction = function(){
+		var superLocalVar = 'Hello, world!';
+		alert(superLocalVar);
+		alert(localVar);
+		alert(arg);		
+		alert(globalVar);
+	}
 
-	alert(localVar);
-	globalVar = 'Global value has beev modified';
+	localFunction();
 }
 
-globalFunction();
-alert(globalVar);
+globalFunction('check scope');
