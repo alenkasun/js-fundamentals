f5bd6832e151a91aad90fb25775e2d7eee7f4fd1
Alyona Shevchenko <fiery.dragon12@gmail.com>
2016-02-10 16:52:59 +0200

    use function expression

--- lesson-4/script.js
+++ lesson-4/script.js
@@ -1,12 +1,12 @@
+var calculatorBar = function (paramOne) {
+	paramOne = paramOne + 3 + 1;
+	paramOne = paramOne * 7;
+
+	return paramOne;
+}
+
 var foo = calculatorBar(2),
 	bar = calculatorBar(3);
-
-function calculatorBar(param) {
-	param = param + 3 + 1;
-	param = param * 7;
-
-	return param;
-}
 
 alert(foo);
 alert(bar);
