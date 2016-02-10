35ef822b6dfb3e70e36cf12b12ef300f09787cea
Alyona Shevchenko <fiery.dragon12@gmail.com>
2016-02-10 15:41:15 +0200

    converting string to number

--- lesson-3/script.js
+++ lesson-3/script.js
@@ -1,4 +1,8 @@
-var foo = "Hello, " + "World!",
-    bar = 4 + 5 + "4";
+var foo = 6 + "",
+    bar = parseInt("34"),      // 34
+    num = parseInt("34s76"),   // 34
+    not = parseInt("s"),       // NaN
+    f1  = parseInt("3.14");    // 3
 
- alert(bar);
+ alert('foo = ' + foo + '  bar = ' + bar);
+ alert('num = ' + num + '  not = ' + not);
