e616fed2942e574c19e2fa418a5cfeb735096026
Alyona Shevchenko <fiery.dragon12@gmail.com>
2016-02-10 17:06:02 +0200

    use function as function argument - way 1

--- lesson-4/script.js
+++ lesson-4/script.js
@@ -1,12 +1,16 @@
-var calculatorBar = function (paramOne) {
+var calculatorBar = function (paramOne, paramTwo, fn) {
 	paramOne = paramOne + 3 + 1;
 	paramOne = paramOne * 7;
 
-	return paramOne;
+	return fn(paramOne, paramTwo);
 }
 
-var foo = calculatorBar(2),
-	bar = calculatorBar(3);
+function sum(paramOne, paramTwo) {
+	return paramOne + paramTwo;
+}
+
+var foo = calculatorBar(2, 2, sum),
+	bar = calculatorBar(3, 3, sum);
 
 alert(foo);
 alert(bar);
