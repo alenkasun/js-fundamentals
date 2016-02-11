54e20a3d39393d58ba143aa3661dab277ba71e86
Alyona Shevchenko <fiery.dragon12@gmail.com>
2016-02-11 15:55:45 +0200

    add new element to array: use index notation or push-method

--- lesson-8/script.js
+++ lesson-8/script.js
@@ -1,5 +1,6 @@
 var arr = [11, "hello", true];
     len = arr.length,
 
-arr[3] = "new element";
-alert(len);
+arr[arr.length] = "a new value";
+arr.push("the best conditions");
+alert(arr[arr.length - 1]);
