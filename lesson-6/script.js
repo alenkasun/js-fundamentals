fbbbd11f0fbd83b7a0e36343c37b1bc51235ca16
Alyona Shevchenko <fiery.dragon12@gmail.com>
2016-02-11 14:05:47 +0200

    search from right to left in strings

--- lesson-6/script.js
+++ lesson-6/script.js
@@ -3,5 +3,8 @@ var obj = "This is a string object.",
 	pos1 = obj.indexOf("This"),
 	pos2 = obj.indexOf("is"),             // 2
 	pos3 = obj.indexOf("is", pos2 + 1);   // 5 
+	pos4 = obj.indexOf("this"),           // -1
+	pos5 = obj.lastIndexOf("is"),         // 5
+	pos6 = obj.lastIndexOf("is", pos5 - 1);
 
-console.log(pos3);
+console.log(pos6);
