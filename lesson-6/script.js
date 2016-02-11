8c94cd73f9bedc2a8713829b2e48ae1f9cd8d191
Alyona Shevchenko <fiery.dragon12@gmail.com>
2016-02-11 14:25:44 +0200

    change strings

--- lesson-6/script.js
+++ lesson-6/script.js
@@ -1,7 +1,6 @@
 var obj = "This is a string object.",
 	length = obj.length,
-	sub1 = obj.substr(0, 4),         // This
-	sub2 = obj.substr(10, 6),        // string
-	sub3 = obj.substr(0, 3);         // Thi
+	newString = obj.replace("object", "value");
 
-console.log(sub3);
+alert(newString);
+alert(obj);
