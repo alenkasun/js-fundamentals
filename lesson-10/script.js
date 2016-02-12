8d3ebe869ea88fc221ec87b7c4b95aea2745c23b
Alyona Shevchenko <fiery.dragon12@gmail.com>
2016-02-12 16:45:54 +0200

    review repeating code: for loop

---
+++ lesson-10/script.js
@@ -0,0 +1,5 @@
+var move = ["up", "down", "forward", "back"];
+
+for (var i = 0; i < move.length; i++) {
+	alert(move[i]);
+}
--- lesson-9/script.js
+++ lesson-9/script.js
@@ -1,5 +1,7 @@
 if (5 < 6) {
 	alert("5 is less than 6");
+} else {
+	alert("This is in the else block");
 }
 
 alert("This is outside of the if statement");
