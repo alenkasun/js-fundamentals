13abd07e487e9c698d7dd4c1c61202929bc01019
Alyona Shevchenko <fiery.dragon12@gmail.com>
2016-02-15 14:47:15 +0200

    add and replace class via pure JS

--- lesson-14/script.js
+++ lesson-14/script.js
@@ -3,11 +3,6 @@
 var divFoo = document.getElementById("foo"),
 	style = divFoo.style;
 
-// style.color = "green";
-style.border = "1px solid black";
-style.backgroundColor = "yellow";
-style.padding = "2px";
-
-alert(style.color);   // empty alert popup
+divFoo.className = "css-class css-class2";
 
 }());
