8094dd814a886d1c04238198f7e556ecdb45fb7d
Alyona Shevchenko <fiery.dragon12@gmail.com>
2016-02-15 13:46:20 +0200

    use innerHTML-method

--- lesson-13/script.js
+++ lesson-13/script.js
@@ -2,14 +2,13 @@
 
 var doc = document,
     el = doc.createElement("p"),
-	content = doc.createTextNode("<strong>This was dynamically created</strong>");
 	pFoo = doc.getElementById("foo");
 
-el.appendChild(content);
-el.setAttribute("align", "center");
+el.innerHTML = "<strong>This is dynamically created.</strong>";
 el.id = "bar";
-
-pFoo.parentNode.insertBefore(el, pFoo);
-//pBar.parentNode.replaceChild(el, pBar);
+pFoo.parentNode.appendChild(el);
+el.innerHTML = el.innerHTML + "<br/ >This was, too";
+el.innerHTML = el.innerHTML + "<br/ >This was, too 2";
+el.innerHTML = el.innerHTML + "<br/ >This was, too 3";
 
 }());
