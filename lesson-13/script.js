c769ad11c3c23b01e8d34645e45dee0d24b6043b
Alyona Shevchenko <fiery.dragon12@gmail.com>
2016-02-15 12:46:17 +0200

    find out difference between findElementsByTagName and querySelectorAll

--- lesson-13/script.js
+++ lesson-13/script.js
@@ -1,11 +1,14 @@
 (function(){
 
-var pElement = document.getElementsByTagName("p");
-alert(pElement.length);  // 5
+var pElement1 = document.getElementsByTagName("p"),
+	pElement2 = document.querySelectorAll("p");
+alert(pElement1.length);  // 5
+alert(pElement2.length);  // 5
 
 var el = document.createElement("p");
 document.body.appendChild(el);
 
-alert(pElement.length);  // 6
+alert(pElement1.length);  // 6
+alert(pElement2.length);  // 5
 
 }());
