bb46429423b8275001fb06a2437abe819a34ad2b
Alyona Shevchenko <fiery.dragon12@gmail.com>
2016-02-15 12:58:44 +0200

    set attributes to DOM-element

--- lesson-13/script.js
+++ lesson-13/script.js
@@ -1,14 +1,13 @@
 (function(){
 
-var pElement1 = document.getElementsByTagName("p"),
-	pElement2 = document.querySelectorAll("p");
-alert(pElement1.length);  // 5
-alert(pElement2.length);  // 5
+var el = document.createElement("p"),
+	content = document.createTextNode("<strong>This was dynamically created</strong>");
 
-var el = document.createElement("p");
+el.appendChild(content);
+el.setAttribute("align", "center");
+el.id = "bar";
+
 document.body.appendChild(el);
 
-alert(pElement1.length);  // 6
-alert(pElement2.length);  // 5
 
 }());
