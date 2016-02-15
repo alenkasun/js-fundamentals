009f9f789fba3c27f7aed280bde435dbc45b0b0c
Alyona Shevchenko <fiery.dragon12@gmail.com>
2016-02-15 11:56:45 +0200

    work with DOM elements

--- lesson-12/script.js
+++ lesson-12/script.js
@@ -1,6 +1,12 @@
 (function(){
 
-var pElements = document.getElementsByTagName("p"); // NodeList
-console.log(pElements);
+var pElements = document.getElementsByTagName("p"), // NodeList
+	lastPElement = pElements[pElements.length - 1];
+
+for (var i = 0, len = pElements.length; i < len; i++) {
+	alert(pElements[i]);
+}
+
+console.log(lastPElement);
 
 }());
