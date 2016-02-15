2bfa166005fd118fd7308dc5dde45b603fd8f559
Alyona Shevchenko <fiery.dragon12@gmail.com>
2016-02-15 11:22:37 +0200

    use window's dialog boxes

--- lesson-11/script.js
+++ lesson-11/script.js
@@ -1,13 +1,9 @@
 (function(){
 
-var foo = "hello, window";
-
-var bar = function() {
-	var foo = "Hello, function";
-
-	alert(window.foo);  // undefined !!!
-};
-
-bar();
+if(confirm("Do you want to go to Google.com?")){
+	alert("We'll take you there.");
+} else {
+	alert("You'll stay here.");
+}
 
 }());
