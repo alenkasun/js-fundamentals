a5076baa54d00d65d8d54820f257fc3c9f57b1c1
Alyona Shevchenko <fiery.dragon12@gmail.com>
2016-02-15 17:04:23 +0200

    determine value of css style property depending on the browser

--- lesson-14/script.js
+++ lesson-14/script.js
@@ -6,7 +6,16 @@ var divFoo = document.getElementById("foo"),
 divFoo.classList.add("css-class");
 divFoo.classList.add("css-class2");
 
-var color = getComputedStyle(divFoo, null).getPropertyValue("color");
+var getStyle = function(el, cssProperty){
+	if(typeof getComputedStyle !== "undefined"){
+		return window.getComputedStyle(el, null).getPropertyValue(cssProperty);
+	} else {
+		return el.currentStyle[cssProperty];
+	}
+};
+
+var color = getStyle(divFoo,"color");
 alert(color);
+
 
 }());
