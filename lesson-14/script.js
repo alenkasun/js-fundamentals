ce1d0f8fa66d9bb2b2fb4e06296b8e6f07ca38cc
Alyona Shevchenko <fiery.dragon12@gmail.com>
2016-02-15 14:09:50 +0200

    use camel-case for modifying style properties in JS

--- lesson-14/script.js
+++ lesson-14/script.js
@@ -4,5 +4,7 @@ var divFoo = document.getElementById("foo");
 
 divFoo.style.color = "green";
 divFoo.style.border = "1px solid black";
+divFoo.style.backgroundColor = "yellow";
+divFoo.style.padding = "2px";
 
 }());
