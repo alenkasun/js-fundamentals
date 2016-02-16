f74e92f8663badab954860336a923f02c3cf3738
Alyona Shevchenko <fiery.dragon12@gmail.com>
2016-02-16 16:10:56 +0200

    review useful first function argument EVT (type, target, etc)

--- lesson-17/script.js
+++ lesson-17/script.js
@@ -1,17 +1,14 @@
 (function(){
 
 	var buttons = document.getElementsByTagName("button");
-	var buttonClick = function() {
-		var className = this.innerHTML.toLowerCase();
-			document.body.className = className;
-	}
 
 
 	for (var i = 0, len = buttons.length; i < len; i++) {
-		buttons[i].addEventListener("click", buttonClick, false);
-		buttons[i].addEventListener("click", function(){alert("hi!!!")}, false);
-
-		buttons[i].removeEventListener("click", buttonClick, false);
+		buttons[i].addEventListener("click", function(e) {
+		var className = this.innerHTML.toLowerCase();
+			document.body.className = className;
+			alert("type: " + e.type + " | target: " + e.target);
+		}, false);
 	}
 
 }());
