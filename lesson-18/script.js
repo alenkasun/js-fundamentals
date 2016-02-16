2baa5e0abbc8972473514e891a01b7fbc69cf8d0
Alyona Shevchenko <fiery.dragon12@gmail.com>
2016-02-16 16:59:51 +0200

    use detachEvent instead removeEventListener for IE8 and below

--- lesson-18/script.js
+++ lesson-18/script.js
@@ -1,11 +1,7 @@
 (function(){
 
 	var links = document.getElementsByTagName("a");
-
-
-	for (var i = 0, len = links.length; i < len; i++) {
-	// use attachEvent instead addEventListener and prefix "on..."" for IE8 and below
-		links[i].attachEvent("onclick", function() {
+	var clickButton = function() {
 			var clasName = event.srcElement.innerHTML.toLowerCase();
 
 			alert(event.type);            // use event
@@ -13,7 +9,12 @@
 			event.returnValue  = false;   // instead event.preventDefault()
 
 			document.body.className = className;
-		});
+	};
+
+	for (var i = 0, len = links.length; i < len; i++) {
+	// use attachEvent(detachEvent) instead addEventListener and prefix "on..."" for IE8 and below
+		links[i].attachEvent("onclick", buttonClick);
+		links[i].detachEvent("onclick", buttonClick);
 	}
 
 }());
