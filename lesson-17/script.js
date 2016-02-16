d4b4b08d2ab6e9946dcb4e184a35fd0d942d40cc
Alyona Shevchenko <fiery.dragon12@gmail.com>
2016-02-16 16:20:25 +0200

    cancel default action via preventDefault

--- lesson-17/script.js
+++ lesson-17/script.js
@@ -1,13 +1,13 @@
 (function(){
 
-	var buttons = document.getElementsByTagName("button");
+	var links = document.getElementsByTagName("a");
 
 
-	for (var i = 0, len = buttons.length; i < len; i++) {
-		buttons[i].addEventListener("click", function(e) {
+	for (var i = 0, len = links.length; i < len; i++) {
+		links[i].addEventListener("click", function(e) {
 		var className = this.innerHTML.toLowerCase();
 			document.body.className = className;
-			alert("type: " + e.type + " | target: " + e.target);
+			e.preventDefault();
 		}, false);
 	}
 
