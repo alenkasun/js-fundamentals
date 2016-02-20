441fd204cd5b62e40c2bc87c5163b9ee4caeb63a
Alyona Shevchenko <alenka_programmer@mail.ru>
2016-02-21 00:51:08 +0200

    remember that all data in localStorage are strings

--- 2 - advanced/lesson-11/script.js
+++ 2 - advanced/lesson-11/script.js
@@ -5,3 +5,11 @@ alert(localStorage.firstName + " " + localStorage.getItem("lastName"));
 
 localStorage.removeItem("lastName");
 alert(localStorage.lastName);    // undefined
+
+localStorage.clear();
+
+
+// Important: in localStorage all data are string
+localStorage.age = 47;
+alert(typeof localStorage.age);   // string
+var age = parseInt(localStorage.age, 10);
