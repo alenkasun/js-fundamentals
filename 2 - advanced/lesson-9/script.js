e73b2027b8d476bcc637cf4a0c7331b222596a4a
Alena Shevchenko <alenka_programmer@mail.ru>
2016-02-20 23:29:58 +0200

    send post-request on server

--- 2 - advanced/lesson-9/script.js
+++ 2 - advanced/lesson-9/script.js
@@ -1,21 +1,43 @@
-var xhr = new XMLHttpRequest();
+var form = document.getElementById("theForm");
 
-xhr.open("GET", "textfile.txt", true);  // async
+eventUtility.addEvent(form, "submit", function(evt){
+	var data = getRequestBody();
+		var xhr = new XMLHttpRequest();
 
-xhr.onreadystatechange = function(){
-	if(xhr.readyState === 4) {
-	 // status: 404 not found
-	 //         500 internal server error
-	 //         200 success
-	 var status = xhr.status;
+	xhr.open("POST", "textfile.txt", true);  // async
+	xhr.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
 
-	 if((status  >= 200 && status < 300)||(status === 304)){
-	 	alert(xhr.responseText);
-	 } else {
-	 	alert("Something bad happened!!!");
-	 }
-}
+	xhr.onreadystatechange = function(){
+		if(xhr.readyState === 4) {
+		// status: 404 not found
+		//         500 internal server error
+		//         200 success
+		var status = xhr.status;
 
-};
+		if((status  >= 200 && status < 300)||(status === 304)){
+			alert(xhr.responseText);
+		} else {
+			alert("Something bad happened!!!");
+		}
+		}
+	};
 
-xhr.send(null);
+	xhr.send(data);
+
+	eventUtility.preventDefault(evt); 
+});
+
+	var getRequestBody = function(){
+	var values = [];
+
+	for(var i = 0, l = form.elements.length; i < l; i++){
+	var el = form.elements[i],
+		name = encodeURIComponent(el.name),
+		value = encodeURIComponent(el.value),
+		complete = name + "=" + value;
+
+		values.push(complete);
+	}
+
+	return values.join("&");
+	};
