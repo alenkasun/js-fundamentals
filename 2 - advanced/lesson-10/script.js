bfa253e2461292b10aa6c34e6c4133ef849909a0
Alena Shevchenko <alenka_programmer@mail.ru>
2016-02-20 23:53:08 +0200

    use json format for client-server exchange

---
+++ 2 - advanced/lesson-10/script.js
@@ -0,0 +1,20 @@
+var xhr = new XMLHttpRequest();
+
+xhr.open("GET", "rss.json.txt", true);  // async
+
+xhr.onreadystatechange = function(){
+	if(xhr.readyState === 4) {
+	// status: 404 not found
+	//         500 internal server error
+	//         200 success
+	var status = xhr.status;
+
+	if((status  >= 200 && status < 300)||(status === 304)){
+		alert(xhr.responseText);
+	} else {
+		alert("Something bad happened!!!");
+	}
+	}
+};
+
+xhr.send(null);
