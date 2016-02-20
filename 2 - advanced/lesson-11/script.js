98a1cb2ceb5041914175073354612d2b52e6872f
Alyona Shevchenko <alenka_programmer@mail.ru>
2016-02-21 01:05:54 +0200

    remember that localStorage is restricted domain

--- 2 - advanced/lesson-11/script.js
+++ 2 - advanced/lesson-11/script.js
@@ -5,8 +5,3 @@ var person = {
 };
 
 localStorage.person = JSON.stringify(person);
-var personObj  = JSON.parse(localStorage.person)
-
-alert(personObj.firstName + " " + personObj.lastName);
-
-localStorage.clear();
