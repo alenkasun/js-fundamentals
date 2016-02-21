09101ff77556476c654259caa41995dd575ec7fa
Alyona Shevchenko <alenka_programmer@mail.ru>
2016-02-21 23:02:42 +0200

    check the checkbox using JS

--- 2 - advanced/lesson-8/script.js
+++ 2 - advanced/lesson-8/script.js
@@ -2,7 +2,7 @@
 
 var form = document.getElementById("theForm"),  // to retrieve data from form elements
     button = form.myButton,
-    select = form.dayOfWeek;       // retrieve button input element by name
+    color = form.color;        // retrieve button input element by name
 
 eventUtility.addEvent(form, "submit", function(){
     eventUtility.preventDefault();
@@ -10,15 +10,9 @@ eventUtility.addEvent(form, "submit", function(){
 
 eventUtility.addEvent(button, "click", function(evt){
     var target = eventUtility.getTarget(evt);
+
+    color.checked = true;
     
-    var index = select.selectedIndex;
-    var option = select.options[index];
-    
-    select.remove(3);
-    var wedOption = new Option("Wednesday", 3);
-    //select.options[select.options.length] = wedOption;
-    //select.options[3] = wedOption;
-    select.add(wedOption, select.options[1]);
 });
 
 }());
